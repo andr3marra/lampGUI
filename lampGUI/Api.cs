8d249a8c2cacbeb0aa97e094ebf11285c059015b
André <andre.marra@live.com>
2020-08-16 19:19:06 -0300

    github

---
+++ lampGUI/Api.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+
+namespace lampGUI {
+    class Api {
+        private WebRequest request;
+        private readonly string url;
+        private string fullUrl;
+        public string endpoint;
+
+        public Api(string url) {
+            this.url = url;
+            request = WebRequest.Create(url);
+        }
+
+        public void SetEndpoint(string endpoint) {
+            fullUrl = url + endpoint + "?";
+        }
+
+        public string Post(byte[] data) {
+
+            request.Method = "POST";
+            request.ContentType = "application/octet-stream";
+            request.ContentLength = data.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(data, 0, data.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            string responseData;
+
+            using (dataStream = response.GetResponseStream()) {
+                StreamReader reader = new StreamReader(dataStream);
+                responseData = reader.ReadToEnd();
+            }
+            response.Close();
+            return responseData;
+        }
+
+        public string Get() {
+
+            request.Method = "GET";
+            request.UseDefaultCredentials = true;
+
+            WebResponse response = request.GetResponse();
+            string responseData;
+
+            using (Stream dataStream = response.GetResponseStream()) {
+                StreamReader reader = new StreamReader(dataStream);
+                responseData = reader.ReadToEnd();
+            }
+            response.Close();
+            return responseData;
+        }
+    }
+}
---
+++ lampGUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lampGUI {
+    static class Program {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Api led = new Api("http://192.168.15.12/color/single?r=0&g=255&b=0");
+            led.Get();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
