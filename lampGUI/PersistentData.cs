8d6ad1dc072773abaeb1e5d1bc490665bef67b28
DESKTOP-F6QQ07O\andre <andre.marra@live.com>
2020-12-02 21:29:17 -0300

    Dll added.

--- lampGUI/PersistentData.cs
+++ lampGUI/PersistentData.cs
@@ -10,33 +10,43 @@ using Un4seen.Bass.Misc;
 using System.Windows.Forms;
 using System.Drawing;
 
-namespace lampGUI {
-    public static class PersistentData {
+namespace lampGUI
+{
+    public static class PersistentData
+    {
         public static Color btnColor4x1 = Color.FromArgb(245, 254, 0);
         public static Color btnColor4x2 = Color.FromArgb(245, 0, 55);
         public static Color btnColor4x3 = Color.FromArgb(245, 57, 75);
         public static bool HideMinimize = false;
         public static List<lamp> lamps = new List<lamp> { };
 
-        public static void Init() {
-            using (StreamReader r = new StreamReader("External/config.json")) {
-                string json = r.ReadToEnd();
-                dynamic array = JsonConvert.DeserializeObject(json);
-                String[] ip = new String[array.Count];
-                for (int i = 0; i < array.Count; i++) {
-                    lamps.Add(new lamp(array[i].name.Value, array[i].ip.Value));
+        public static void Init()
+        {
+            if (File.Exists("External/config.json"))
+            {
+                using (StreamReader r = new StreamReader("External/config.json"))
+                {
+                    string json = r.ReadToEnd();
+                    dynamic array = JsonConvert.DeserializeObject(json);
+                    String[] ip = new String[array.Count];
+                    for (int i = 0; i < array.Count; i++)
+                    {
+                        lamps.Add(new lamp(array[i].name.Value, array[i].ip.Value));
+                    }
                 }
             }
         }
     }
-    public class lamp {
+    public class lamp
+    {
         public String name;
         public String ip;
         public byte brightness;
         public int delay;
         public bool selected;
         public char mode;
-        public lamp(String name, String ip) {
+        public lamp(String name, String ip)
+        {
             this.name = name;
             this.ip = ip;
             this.brightness = 255;
